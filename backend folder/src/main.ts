1af97206d7a64f07d8642af20ec9b9ec2bbbf4f9
EUNICE JACOB <115852649+eunicegigijacob@users.noreply.github.com>
2023-05-26 12:12:46 +0100

    edits

--- backend folder/src/main.ts
+++ backend folder/src/main.ts
@@ -11,6 +11,8 @@ async function bootstrap() {
       callback(null, true);
     },
   });
-  await app.listen(3000);
+  await app.listen(3000, () => {
+    console.log(`app listening on port 3000`);
+  });
 }
 bootstrap();
